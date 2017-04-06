376a91e26b503a601936ce4326d8499cb45ee01f
commit-queue@webkit.org <commit-queue@webkit.org@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2017-04-06 15:23:53 +0000

    [JSC][MIPS][DFG] Use x86 generic HasOwnProperty https://bugs.webkit.org/show_bug.cgi?id=170222
    Patch by Guillaume Emont <guijemont@igalia.com> on 2017-04-06
    Reviewed by Yusuke Suzuki.
    
    * dfg/DFGFixupPhase.cpp:
    (JSC::DFG::FixupPhase::fixupNode):
    use the X86 special version for HasOwnProperty on MIPS too.
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    use the X86 special version for HasOwnProperty on MIPS too.
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@215038 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
+++ Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
@@ -5026,7 +5026,7 @@ void SpeculativeJIT::compile(Node* node)
         break;
 
     case HasOwnProperty: {
-#if CPU(X86)
+#if CPU(X86) || CPU(MIPS)
         ASSERT(node->child2().useKind() == UntypedUse);
         SpeculateCellOperand object(this, node->child1());
         JSValueOperand key(this, node->child2());
