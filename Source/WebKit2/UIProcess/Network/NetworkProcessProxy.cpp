fberlakovich/r2c-webkit

WebContent processes crash when the network process crashes with pending connection requests https://bugs.webkit.org/show_bug.cgi?id=174065 <rdar://problem/30359835>
Reviewed by Tim Horton.

Source/WebKit2:

The bug was caused by the UI process clearing away pending network connection requests whenever the existing
network process crashed. This resulted in WebContent process killing itself inside ensureNetworkProcessConnection.

Fixed the bug by re-launching a new network process when this happens. We don't try to re-launch a new process
if the previous attempt to launch a network process had failed.

This patch splits NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch into networkProcessFailedToLaunch
and networkProcessCrashed to differentiate those two cases, and invoke the respective callbacks in WebProcessPool.

* UIProcess/API/Cocoa/WKProcessPool.mm:
(-[WKProcessPool _networkProcessIdentifier]): Added.
* UIProcess/API/Cocoa/WKProcessPoolPrivate.h:
* UIProcess/Network/NetworkProcessProxy.cpp:
(WebKit::NetworkProcessProxy::networkProcessCrashed): Added.
(WebKit::NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch): Split into the two following functions.
(WebKit::NetworkProcessProxy::networkProcessFailedToLaunch): Extracted from networkProcessCrashedOrFailedToLaunch.
(WebKit::NetworkProcessProxy::clearCallbackStates): Extracted from networkProcessCrashedOrFailedToLaunch
(WebKit::NetworkProcessProxy::didClose): Call networkProcessCrashed.
(WebKit::NetworkProcessProxy::didFinishLaunching): Call networkProcessFailedToLaunch.
* UIProcess/Network/NetworkProcessProxy.h:
* UIProcess/WebProcessPool.cpp:
(WebKit::WebProcessPool::networkProcessCrashed): Start a new network process when there are pending connection
requests the crached network failed to fullfil.
(WebKit::WebProcessPool::networkProcessFailedToLaunch):
* UIProcess/WebProcessPool.h:

Tools:

Add a API to ensure UI process tries to relaunch a new network process when the network process
crashes w

Source/WebKit2/UIProcess/Network/NetworkProcessProxy.cpp
@@ -164,7 +164,20 @@ void NetworkProcessProxy::deleteWebsiteDataForOrigins(SessionID sessionID, Optio
     send(Messages::NetworkProcess::DeleteWebsiteDataForOrigins(sessionID, dataTypes, origins, cookieHostNames, callbackID), 0);
 }
 
-void NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch()
+void NetworkProcessProxy::networkProcessCrashed()
+{
+    clearCallbackStates();
+
+    Vector<Ref<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>> pendingReplies;
+    pendingReplies.reserveInitialCapacity(m_pendingConnectionReplies.size());
+    for (auto& reply : m_pendingConnectionReplies)
+        pendingReplies.append(WTFMove(reply));
+
+    // Tell the network process manager to forget about this network process proxy. This may cause us to be deleted.
+    m_processPool.networkProcessCrashed(*this, WTFMove(pendingReplies));
+}
+
+void NetworkProcessProxy::networkProcessFailedToLaunch()
 {
     // The network process must have crashed or exited, send any pending sync replies we might have.
     while (!m_pendingConnectionReplies.isEmpty()) {
@@ -178,7 +191,13 @@ void NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch()
         notImplemented();
 #endif
     }
+    clearCallbackStates();
+    // Tell the network process manager to forget about this network process proxy. This may cause us to be deleted.
+    m_processPool.networkProcessFailedToLaunch(*this);
+}
 
+void NetworkProcessProxy::clearCallbackStates()
+{
     for (const auto& callback : m_pendingFetchWebsiteDataCallbacks.values())
         callback(WebsiteData());
     m_pendingFetchWebsiteDataCallbacks.clear();
@@ -190,9 +209,6 @@ void NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch()
     for (const auto& callback : m_pendingDeleteWebsiteDataForOriginsCallbacks.values())
         callback();
     m_pendingDeleteWebsiteDataForOriginsCallbacks.clear();
-
-    // Tell the network process manager to forget about this network process proxy. This may cause us to be deleted.
-    m_processPool.networkProcessCrashed(this);
 }
 
 void NetworkProcessProxy::didReceiveMessage(IPC::Connection& connection, IPC::Decoder& decoder)
@@ -223,7 +239,7 @@ void NetworkProcessProxy::didClose(IPC::Connection&)
     m_tokenForHoldingLockedFiles = nullptr;
 
     // This may cause us to be deleted.
-    networkProcessCrashedOrFailedToLaunch();
+    networkProcessCrashed();
 }
 
 void NetworkProcessProxy::didReceiveInvalidMessage(IPC::Connection&, IPC::StringReference, IPC::StringReference)
@@ -302,7 +318,7 @@ void NetworkProcessProxy::didFinishLaunching(ProcessLauncher* launcher, IPC::Con
     ChildProcessProxy::didFinishLaunching(launcher, connectionIdentifier);
 
     if (IPC::Connection::identifierIsNull(connectionIdentifier)) {
-        networkProcessCrashedOrFailedToLaunch();
+        networkProcessFailedToLaunch();
         return;
     }
 

Source/WebKit2/UIProcess/WebProcessPool.cpp
@@ -490,14 +490,25 @@ NetworkProcessProxy& WebProcessPool::ensureNetworkProcess(WebsiteDataStore* with
     return *m_networkProcess;
 }
 
-void WebProcessPool::networkProcessCrashed(NetworkProcessProxy* networkProcessProxy)
+void WebProcessPool::networkProcessCrashed(NetworkProcessProxy& networkProcessProxy, Vector<Ref<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>>&& pendingReplies)
+{
+    networkProcessFailedToLaunch(networkProcessProxy);
+    ASSERT(!m_networkProcess);
+    if (pendingReplies.isEmpty())
+        return;
+    auto& newNetworkProcess = ensureNetworkProcess();
+    for (auto& reply : pendingReplies)
+        newNetworkProcess.getNetworkProcessConnection(WTFMove(reply));
+}
+
+void WebProcessPool::networkProcessFailedToLaunch(NetworkProcessProxy& networkProcessProxy)
 {
     ASSERT(m_networkProcess);
-    ASSERT(networkProcessProxy == m_networkProcess.get());
+    ASSERT(&networkProcessProxy == m_networkProcess.get());
     m_didNetworkProcessCrash = true;
 
     for (auto& supplement : m_supplements.values())
-        supplement->processDidClose(networkProcessProxy);
+        supplement->processDidClose(&networkProcessProxy);
 
     m_client.networkProcessDidCrash(this);
 

Source/WebKit2/UIProcess/WebProcessPool.h
@@ -315,7 +315,8 @@ public:
     // Network Process Management
     NetworkProcessProxy& ensureNetworkProcess(WebsiteDataStore* withWebsiteDataStore = nullptr);
     NetworkProcessProxy* networkProcess() { return m_networkProcess.get(); }
-    void networkProcessCrashed(NetworkProcessProxy*);
+    void networkProcessCrashed(NetworkProcessProxy&, Vector<Ref<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>>&&);
+    void networkProcessFailedToLaunch(NetworkProcessProxy&);
 
     void getNetworkProcessConnection(Ref<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>&&);
 

vivienne-w/WPEWebKit

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@219087 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/Network/NetworkProcessProxy.cpp
@@ -165,7 +165,20 @@ void NetworkProcessProxy::deleteWebsiteDataForOrigins(SessionID sessionID, Optio
     send(Messages::NetworkProcess::DeleteWebsiteDataForOrigins(sessionID, dataTypes, origins, cookieHostNames, callbackID), 0);
 }
 
-void NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch()
+void NetworkProcessProxy::networkProcessCrashed()
+{
+    clearCallbackStates();
+
+    Vector<RefPtr<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>> pendingReplies;
+    pendingReplies.reserveInitialCapacity(m_pendingConnectionReplies.size());
+    for (auto& reply : m_pendingConnectionReplies)
+        pendingReplies.append(WTFMove(reply));
+
+    // Tell the network process manager to forget about this network process proxy. This may cause us to be deleted.
+    m_processPool.networkProcessCrashed(*this, WTFMove(pendingReplies));
+}
+
+void NetworkProcessProxy::networkProcessFailedToLaunch()
 {
     // The network process must have crashed or exited, send any pending sync replies we might have.
     while (!m_pendingConnectionReplies.isEmpty()) {
@@ -179,7 +192,13 @@ void NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch()
         notImplemented();
 #endif
     }
+    clearCallbackStates();
+    // Tell the network process manager to forget about this network process proxy. This may cause us to be deleted.
+    m_processPool.networkProcessFailedToLaunch(*this);
+}
 
+void NetworkProcessProxy::clearCallbackStates()
+{
     for (const auto& callback : m_pendingFetchWebsiteDataCallbacks.values())
         callback(WebsiteData());
     m_pendingFetchWebsiteDataCallbacks.clear();
@@ -191,9 +210,6 @@ void NetworkProcessProxy::networkProcessCrashedOrFailedToLaunch()
     for (const auto& callback : m_pendingDeleteWebsiteDataForOriginsCallbacks.values())
         callback();
     m_pendingDeleteWebsiteDataForOriginsCallbacks.clear();
-
-    // Tell the network process manager to forget about this network process proxy. This may cause us to be deleted.
-    m_processPool.networkProcessCrashed(this);
 }
 
 void NetworkProcessProxy::didReceiveMessage(IPC::Connection& connection, IPC::Decoder& decoder)
@@ -223,7 +239,7 @@ void NetworkProcessProxy::didClose(IPC::Connection&)
     m_tokenForHoldingLockedFiles = nullptr;
 
     // This may cause us to be deleted.
-    networkProcessCrashedOrFailedToLaunch();
+    networkProcessCrashed();
 }
 
 void NetworkProcessProxy::didReceiveInvalidMessage(IPC::Connection&, IPC::StringReference, IPC::StringReference)
@@ -295,7 +311,7 @@ void NetworkProcessProxy::didFinishLaunching(ProcessLauncher* launcher, IPC::Con
     ChildProcessProxy::didFinishLaunching(launcher, connectionIdentifier);
 
     if (IPC::Connection::identifierIsNull(connectionIdentifier)) {
-        networkProcessCrashedOrFailedToLaunch();
+        networkProcessFailedToLaunch();
         return;
     }
 

Source/WebKit2/UIProcess/WebProcessPool.cpp
@@ -401,14 +401,25 @@ NetworkProcessProxy& WebProcessPool::ensureNetworkProcess()
     return *m_networkProcess;
 }
 
-void WebProcessPool::networkProcessCrashed(NetworkProcessProxy* networkProcessProxy)
+void WebProcessPool::networkProcessCrashed(NetworkProcessProxy& networkProcessProxy, Vector<RefPtr<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>>&& pendingReplies)
+{
+    networkProcessFailedToLaunch(networkProcessProxy);
+    ASSERT(!m_networkProcess);
+    if (pendingReplies.isEmpty())
+        return;
+    auto& newNetworkProcess = ensureNetworkProcess();
+    for (auto& reply : pendingReplies)
+        newNetworkProcess.getNetworkProcessConnection(WTFMove(reply));
+}
+
+void WebProcessPool::networkProcessFailedToLaunch(NetworkProcessProxy& networkProcessProxy)
 {
     ASSERT(m_networkProcess);
-    ASSERT(networkProcessProxy == m_networkProcess.get());
+    ASSERT(&networkProcessProxy == m_networkProcess.get());
     m_didNetworkProcessCrash = true;
 
     for (auto& supplement : m_supplements.values())
-        supplement->processDidClose(networkProcessProxy);
+        supplement->processDidClose(&networkProcessProxy);
 
     m_client.networkProcessDidCrash(this);
 

Source/WebKit2/UIProcess/WebProcessPool.h
@@ -283,7 +283,8 @@ public:
     // Network Process Management
     NetworkProcessProxy& ensureNetworkProcess();
     NetworkProcessProxy* networkProcess() { return m_networkProcess.get(); }
-    void networkProcessCrashed(NetworkProcessProxy*);
+    void networkProcessCrashed(NetworkProcessProxy&, Vector<RefPtr<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>>&&);
+    void networkProcessFailedToLaunch(NetworkProcessProxy&);
 
     void getNetworkProcessConnection(PassRefPtr<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>);
 