73cc2b458d7636535c4cd4b1fce6d7b9ce91cbd7
Enrique Ocaña González <eocanha@igalia.com>
2020-12-16 17:40:48 +0000

    Revert "[GStreamer][MSE][EME] Don't inject a protection events already injected before"
    This reverts commit 453c033eb71226b7b858418bf44ed23b7c179f9c.

--- Source/WebCore/platform/graphics/gstreamer/mse/AppendPipeline.h
+++ Source/WebCore/platform/graphics/gstreamer/mse/AppendPipeline.h
@@ -168,8 +168,7 @@ private:
     struct PadProbeInformation m_appsinkPadProtectionProbeInformation;
     GValue m_cachedProtectionEvents;
     bool m_isProcessingProtectionEvents { false };
-    GRefPtr<GstEvent> m_protectionEventToInject;
-    bool m_isProtectionEventAlreadyInjected { false };
+    GRefPtr<GstEvent> m_protectionEventPendingToInject;
 #endif
     // Keeps track of the states of append processing, to avoid performing actions inappropriate for the current state
     // (eg: processing more samples when the last one has been detected, etc.). See setAppendState() for valid
