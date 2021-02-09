bac63ff41946c693b7b93c0603a0d838e318b3be
Eugene Mutavchi <Ievgen_Mutavchi@comcast.com>
2021-02-09 03:37:39 +0000

    Fix memory pressure poller thread leak

--- Source/WTF/wtf/MemoryPressureHandler.h
+++ Source/WTF/wtf/MemoryPressureHandler.h
@@ -204,6 +204,8 @@ private:
     RunLoop::Timer<MemoryPressureHandler> m_holdOffTimer;
     void holdOffTimerFired();
 
+    struct MemoryUsagePollerThreadContext;
+
     class MemoryUsagePoller {
         WTF_MAKE_NONCOPYABLE(MemoryUsagePoller); WTF_MAKE_FAST_ALLOCATED;
     public:
@@ -212,6 +214,7 @@ private:
 
     private:
         RefPtr<Thread> m_thread;
+        RefPtr<MemoryUsagePollerThreadContext> m_context;
     };
 
     std::unique_ptr<MemoryUsagePoller> m_memoryUsagePoller;
