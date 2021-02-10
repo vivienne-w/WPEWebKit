fberlakovich/r2c-webkit

REGRESSION: (r257905) [ Mac wk2 Debug ] ASSERTION FAILED: !m_isolatedWorld->isNormal() || m_wrapper || !m_jsFunction https://bugs.webkit.org/show_bug.cgi?id=208642
Reviewed by Darin Adler.

This patch fixes stale assertions and comments in JSEventListener.h, which has various problems.

1. This assertion is saying, "If m_wrapper is dead, m_jsFunction must be dead". This is wrong. Given that we have conservative
   GC, JSC never guarantees such a condition. Even if m_wrapper is dead, m_jsFunction can be alive by various reasons: conservative
   GC finds it, user code stores this function somewhere reachable from the root, etc.
   The reason why this wrong assertion exists here is because the JSEventListener code and assertion assume that m_jsFunction is nullptr
   when it is not initialized, and once it is initialized, it should be non nullptr. This is wrong because Weak<> can collect it if it
   is not retained. This `!m_jsFunction` check mixes "it is not initialized" and "it is already initialized but collected".
   The correct assertion should be checking `m_wrapper` and `m_jsFunction` are alive (not checking deadness, which is not guaranteed) if
   the event-listener is once initialized. This patch adds m_isInitialized member to track this status separately from `m_wrapper` and
   `m_jsFunction`.
2. JSEventListener::jsFunction has `if (!m_jsFunction)` condition. But this is not correct. This can revive JSFunction if it is collected
   because m_wrapper is gone or some way, but this is not expected behavior. The correct way is checking `m_isInitialized`. Once the event-listener
   is initialized, keeping m_wrapper and m_jsFunction alive is the responsibility of JSEventListener's owner.
3. The comments about "zombie m_jsFunctions" is wrong. We are using JSC::Weak<>. So if the object gets collected, it returns
   nullptr, not getting a zombie pointer.
4. We are emitting write-barrier in a wrong order. In the heavily stressed scenario, it is possible that concurrent 

Source/WebCore/bindings/js/JSLazyEventListener.cpp
@@ -173,7 +173,7 @@ JSObject* JSLazyEventListener::initializeJSFunction(ScriptExecutionContext& exec
         if (!wrapper()) {
             // Ensure that 'node' has a JavaScript wrapper to mark the event listener we're creating.
             // FIXME: Should pass the global object associated with the node
-            setWrapper(vm, asObject(toJS(lexicalGlobalObject, globalObject, *m_originalNode)));
+            setWrapperWhenInitializingJSFunction(vm, asObject(toJS(lexicalGlobalObject, globalObject, *m_originalNode)));
         }
 
         // Add the event's home element to the scope

vivienne-w/WPEWebKit

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@258189 268f45cc-cd09-0410-ab3c-d52691b4dbfc
NOTE (eocanha):
This patch solves a Disney+ issue with JS code that directly accesses
mediaSource.sourceBuffers from JavaScript to register an event listener on the
addsourcebuffer event, but when the event happened, the event listener had
been declared "dead" by the garbage collector and never ran. If we held
mediaSource.sourceBuffers in a temporary variable in an outer scope, the
behaviour didn't happen.

Source/WebCore/bindings/js/JSLazyEventListener.cpp
@@ -148,7 +148,7 @@ JSObject* JSLazyEventListener::initializeJSFunction(ScriptExecutionContext& exec
         if (!wrapper()) {
             // Ensure that 'node' has a JavaScript wrapper to mark the event listener we're creating.
             // FIXME: Should pass the global object associated with the node
-            setWrapper(vm, asObject(toJS(exec, globalObject, *m_originalNode)));
+            setWrapperWhenInitializingJSFunction(vm, asObject(toJS(exec, globalObject, *m_originalNode)));
         }
 
         // Add the event's home element to the scope