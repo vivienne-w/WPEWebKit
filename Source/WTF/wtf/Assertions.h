1c80c20353105bc4da8b058f5e4b61d55f9d8483
Basuke Suzuki <basuke.suzuki@sony.com>
2021-10-03 17:19:59 +0000

    Enable release log to stderr https://bugs.webkit.org/show_bug.cgi?id=230725 <rdar://problem/83740529>
    Reviewed by Michael Catanzaro.
    
    .:
    
    Introduced new flags, USE_LOG_STDERR for release logging to stderr.
    
    * Source/cmake/WebKitFeatures.cmake:
    
    Source/WebCore:
    
    SQLiteDatabase uses hard-coded %{public} format specifiers. Replace them with defined
    macro.
    
    No new tests because there is no behavior change.
    
    * platform/sql/SQLiteDatabase.cpp:
    (WebCore::SQLiteDatabase::close):
    (WebCore::SQLiteDatabase::prepareStatementSlow):
    (WebCore::SQLiteDatabase::prepareStatement):
    (WebCore::SQLiteDatabase::prepareHeapStatementSlow):
    (WebCore::SQLiteDatabase::prepareHeapStatement):
    
    Source/WTF:
    
    Define new compiler definitions, USE_LOG_STDERR for release logging. We don't have
    modern logging backend so that dumping out to stdout/stderr is still very valuable.
    
    * wtf/Assertions.cpp:
    * wtf/Assertions.h:
    * wtf/Logger.h:
    (WTF::Logger::log):
    (WTF::Logger::logVerbose):
    
    Canonical link: https://commits.webkit.org/242446@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@283469 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Assertions.h
+++ Source/WTF/wtf/Assertions.h
@@ -95,7 +95,9 @@ extern "C" void _ReadWriteBarrier(void);
 #define LOG_DISABLED !ASSERT_ENABLED
 #endif
 
-#ifndef RELEASE_LOG_DISABLED
+#if ENABLE(RELEASE_LOG)
+#define RELEASE_LOG_DISABLED 0
+#else
 #define RELEASE_LOG_DISABLED !(USE(OS_LOG) || USE(JOURNALD))
 #endif
 
@@ -163,9 +165,9 @@ typedef struct {
     WTFLogLevel level;
 #if !RELEASE_LOG_DISABLED
     const char* subsystem;
-#endif
-#if USE(OS_LOG) && !RELEASE_LOG_DISABLED
+#if USE(OS_LOG)
     __unsafe_unretained os_log_t osLogChannel;
+#endif
 #endif
 } WTFLogChannel;
 
@@ -178,6 +180,8 @@ typedef struct {
 #define LOG_CHANNEL_WEBKIT_SUBSYSTEM "WebKitGTK"
 #elif PLATFORM(WPE)
 #define LOG_CHANNEL_WEBKIT_SUBSYSTEM "WPEWebKit"
+#elif PLATFORM(PLAYSTATION)
+#define LOG_CHANNEL_WEBKIT_SUBSYSTEM "SceNKWebKit"
 #else
 #define LOG_CHANNEL_WEBKIT_SUBSYSTEM "com.apple.WebKit"
 #endif
@@ -189,12 +193,10 @@ typedef struct {
 #if RELEASE_LOG_DISABLED
 #define DEFINE_LOG_CHANNEL(name, subsystem) \
     WTFLogChannel LOG_CHANNEL(name) = { (WTFLogChannelState)0, #name, (WTFLogLevel)1 };
-#endif
-#if USE(OS_LOG) && !RELEASE_LOG_DISABLED
+#elif USE(OS_LOG)
 #define DEFINE_LOG_CHANNEL(name, subsystem) \
     WTFLogChannel LOG_CHANNEL(name) = { (WTFLogChannelState)0, #name, (WTFLogLevel)1, subsystem, OS_LOG_DEFAULT };
-#endif
-#if USE(JOURNALD) && !RELEASE_LOG_DISABLED
+#else
 #define DEFINE_LOG_CHANNEL(name, subsystem)                             \
     WTFLogChannel LOG_CHANNEL(name) = { (WTFLogChannelState)0, #name, (WTFLogLevel)1, subsystem };
 #endif
@@ -520,8 +522,7 @@ constexpr bool assertionFailureDueToUnreachableCode = false;
 #define RELEASE_LOG_WITH_LEVEL_IF(isAllowed, channel, level, ...) do { if (isAllowed) RELEASE_LOG_WITH_LEVEL(channel, level, __VA_ARGS__); } while (0)
 
 #define RELEASE_LOG_STACKTRACE(channel) ((void)0)
-#endif
-#if USE(OS_LOG) && !RELEASE_LOG_DISABLED
+#elif USE(OS_LOG)
 #define PUBLIC_LOG_STRING "{public}s"
 #define RELEASE_LOG(channel, ...) os_log(LOG_CHANNEL(channel).osLogChannel, __VA_ARGS__)
 #define RELEASE_LOG_ERROR(channel, ...) os_log_error(LOG_CHANNEL(channel).osLogChannel, __VA_ARGS__)
@@ -537,9 +538,9 @@ constexpr bool assertionFailureDueToUnreachableCode = false;
     if ((isAllowed) && LOG_CHANNEL(channel).level >= (logLevel)) \
         os_log(LOG_CHANNEL(channel).osLogChannel, __VA_ARGS__); \
 } while (0)
-#endif
 
-#if USE(JOURNALD) && !RELEASE_LOG_DISABLED
+#elif USE(JOURNALD)
+
 #define PUBLIC_LOG_STRING "s"
 #define SD_JOURNAL_SEND(channel, priority, file, line, function, ...) sd_journal_send_with_location("CODE_FILE=" file, "CODE_LINE=" line, function, "WEBKIT_SUBSYSTEM=%s", LOG_CHANNEL(channel).subsystem, "WEBKIT_CHANNEL=%s", LOG_CHANNEL(channel).name, "PRIORITY=%i", priority, "MESSAGE=" __VA_ARGS__, nullptr)
 
@@ -559,6 +560,32 @@ constexpr bool assertionFailureDueToUnreachableCode = false;
     if ((isAllowed) && LOG_CHANNEL(channel).level >= (logLevel)) \
         SD_JOURNAL_SEND(channel, LOG_INFO, __FILE__, _STRINGIFY(__LINE__), __func__, __VA_ARGS__); \
 } while (0)
+
+#else
+
+#define PUBLIC_LOG_STRING "s"
+#define PRIVATE_LOG_STRING "s"
+#define LOGF(channel, priority, fmt, ...) do { \
+    auto& logChannel = LOG_CHANNEL(channel); \
+    if (logChannel.state != WTFLogChannelState::Off) \
+        fprintf(stderr, "[%s:%s:%i] " fmt "\n", logChannel.subsystem, logChannel.name, priority, ##__VA_ARGS__); \
+} while (0)
+
+#define RELEASE_LOG(channel, ...) LOGF(channel, 4, __VA_ARGS__)
+#define RELEASE_LOG_ERROR(channel, ...) LOGF(channel, 1, __VA_ARGS__)
+#define RELEASE_LOG_FAULT(channel, ...) LOGF(channel, 2, __VA_ARGS__)
+#define RELEASE_LOG_INFO(channel, ...) LOGF(channel, 3, __VA_ARGS__)
+
+#define RELEASE_LOG_WITH_LEVEL(channel, logLevel, ...) do { \
+    if (LOG_CHANNEL(channel).level >= (logLevel)) \
+        LOGF(channel, logLevel, __VA_ARGS__); \
+} while (0)
+
+#define RELEASE_LOG_WITH_LEVEL_IF(isAllowed, channel, logLevel, ...) do { \
+    if ((isAllowed) && LOG_CHANNEL(channel).level >= (logLevel)) \
+        LOGF(channel, logLevel, __VA_ARGS__); \
+} while (0)
+
 #endif
 
 #if !RELEASE_LOG_DISABLED
