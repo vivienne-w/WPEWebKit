0aa355cbca7375ba6258ba3b81fde465f6626a0c
Enrique Ocaña González <eocanha@igalia.com>
2022-02-15 19:48:37 +0000

    [BCM][GStreamer] Correct buffering level with playpump content in Nexus
    Playpump can buffer a lot of content on its own, draining GstQueue2 and
    resulting in buffering levels that don't reflect reality. This behaviour can
    cause slow starts (GstQueue2 takes longer to fill up if playpump is draining
    it) or even rebuffering pauses when the truth is that there's actually plenty
    of buffered content (in playpump) that isn't being had into account.

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -1379,6 +1379,13 @@ void MediaPlayerPrivateGStreamer::handleMessage(GstMessage* message)
         }
 #endif
 
+#if PLATFORM(BCM_NEXUS) || PLATFORM(BROADCOM)
+        if (currentState == GST_STATE_NULL && newState == GST_STATE_READY && g_strstr_len(GST_MESSAGE_SRC_NAME(message), 13, "brcmvidfilter"))
+            m_vidfilter = GST_ELEMENT(GST_MESSAGE_SRC(message));
+        else if (currentState == GST_STATE_READY && newState == GST_STATE_NULL && g_strstr_len(GST_MESSAGE_SRC_NAME(message), 13, "brcmvidfilter"))
+            m_vidfilter = nullptr;
+#endif
+
         if (!messageSourceIsPlaybin || m_delayingLoad)
             break;
         updateStates();
@@ -1570,10 +1577,27 @@ void MediaPlayerPrivateGStreamer::processBufferingStats(GstMessage* message)
 #if ENABLE(MEDIA_STREAM) && !GST_CHECK_VERSION(1, 16, 0)
     if (m_streamPrivate) return;
 #endif
-
     m_buffering = true;
     gst_message_parse_buffering(message, &m_bufferingPercentage);
 
+#if PLATFORM(BCM_NEXUS) || PLATFORM(BROADCOM)
+    // The Nexus playpump buffers a lot of data. Let's add it as if it had been buffered by the GstQueue2
+    // (only when using in-memory buffering), so we get more realistic percentages.
+    if (!m_downloadBuffer && m_vidfilter) {
+        int originalBufferingPercentage = m_bufferingPercentage;
+        GstObject *queue2 = GST_MESSAGE_SRC(message);
+        guint maxSizeBytes = 0;
+        // Current-level-bytes seems to be inacurate, so we compute its value from the buffering percentage
+        g_object_get(queue2, "max-size-bytes", &maxSizeBytes, nullptr);
+
+        guint playpumpBufferedBytes = 0;
+        g_object_get(GST_OBJECT(m_vidfilter.get()), "buffered-bytes", &playpumpBufferedBytes, nullptr);
+
+        size_t currentLevelBytes = (size_t)maxSizeBytes * (size_t)m_bufferingPercentage / (size_t)100 + (size_t)playpumpBufferedBytes;
+        m_bufferingPercentage = currentLevelBytes > maxSizeBytes ? 100 : (int)(currentLevelBytes * 100 / maxSizeBytes);
+        GST_DEBUG("[Buffering] Buffering: %d%% (corrected to %d%% with playpump content).", originalBufferingPercentage, m_bufferingPercentage);
+    } else
+#endif
     GST_DEBUG("[Buffering] Buffering: %d%%.", m_bufferingPercentage);
 
     if (m_bufferingPercentage == 100)
