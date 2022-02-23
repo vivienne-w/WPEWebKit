WebKit/WebKit-http

[WPE] Improve garbage collecting

Source/WTF/wtf/linux/MemoryPressureHandlerLinux.cpp
@@ -316,7 +316,7 @@ void MemoryPressureHandler::pollMemoryPressure()
 
     MemoryPressureHandler::singleton().setUnderMemoryPressure(critical);
     callOnMainThread([critical] {
-        MemoryPressureHandler::singleton().respondToMemoryPressure(critical ? Critical::Yes : Critical::No);
+        MemoryPressureHandler::singleton().respondToMemoryPressure(critical ? Critical::Yes : Critical::No, critical ? Synchronous::Yes : Synchronous::No);
     });
 }
 
@@ -397,9 +397,9 @@ void MemoryPressureHandler::install()
 
         setUnderMemoryPressure(critical);
         if (isMainThread())
-            respondToMemoryPressure(critical ? Critical::Yes : Critical::No);
+            respondToMemoryPressure(critical ? Critical::Yes : Critical::No, critical ? Synchronous::Yes : Synchronous::No);
         else
-            RunLoop::main().dispatch([this, critical] { respondToMemoryPressure(critical ? Critical::Yes : Critical::No); });
+            RunLoop::main().dispatch([this, critical] { respondToMemoryPressure(critical ? Critical::Yes : Critical::No, critical ? Synchronous::Yes : Synchronous::No); });
     });
 
     if (ReliefLogger::loggingEnabled() && isUnderMemoryPressure())

Source/WebCore/page/MemoryRelease.cpp
@@ -71,6 +71,10 @@ static void releaseNoncriticalMemory()
     MemoryCache::singleton().pruneDeadResourcesToSize(0);
 
     InlineStyleSheetOwner::clearCache();
+
+#if PLATFORM(WPE)
+    GCController::singleton().garbageCollectSoon();
+#endif
 }
 
 static void releaseCriticalMemory(Synchronous synchronous)
@@ -102,7 +106,7 @@ static void releaseCriticalMemory(Synchronous synchronous)
     if (synchronous == Synchronous::Yes) {
         GCController::singleton().garbageCollectNow();
     } else {
-#if PLATFORM(IOS)
+#if PLATFORM(IOS) || PLATFORM(WPE)
         GCController::singleton().garbageCollectNowIfNotDoneRecently();
 #else
         GCController::singleton().garbageCollectSoon();

vivienne-w/WPEWebKit

Source/WTF/wtf/linux/MemoryPressureHandlerLinux.cpp
@@ -275,10 +275,10 @@ void MemoryPressureHandler::triggerMemoryPressureEvent(bool isCritical)
     setUnderMemoryPressure(true);
 
     if (isMainThread())
-        respondToMemoryPressure(isCritical ? Critical::Yes : Critical::No);
+        respondToMemoryPressure(isCritical ? Critical::Yes : Critical::No, isCritical ? Synchronous::Yes : Synchronous::No);
     else
         RunLoop::main().dispatch([this, isCritical] {
-            respondToMemoryPressure(isCritical ? Critical::Yes : Critical::No);
+            respondToMemoryPressure(isCritical ? Critical::Yes : Critical::No, isCritical ? Synchronous::Yes : Synchronous::No);
         });
 
     if (ReliefLogger::loggingEnabled() && isUnderMemoryPressure())

Source/WebCore/page/MemoryRelease.cpp
@@ -82,6 +82,10 @@ static void releaseNoncriticalMemory(MaintainMemoryCache maintainMemoryCache)
         MemoryCache::singleton().pruneDeadResourcesToSize(0);
 
     InlineStyleSheetOwner::clearCache();
+
+#if PLATFORM(WPE)
+    GCController::singleton().garbageCollectSoon();
+#endif
 }
 
 static void releaseCriticalMemory(Synchronous synchronous, MaintainBackForwardCache maintainBackForwardCache, MaintainMemoryCache maintainMemoryCache)
@@ -117,7 +121,7 @@ static void releaseCriticalMemory(Synchronous synchronous, MaintainBackForwardCa
     if (synchronous == Synchronous::Yes) {
         GCController::singleton().garbageCollectNow();
     } else {
-#if PLATFORM(IOS_FAMILY)
+#if PLATFORM(IOS_FAMILY) || PLATFORM(WPE)
         GCController::singleton().garbageCollectNowIfNotDoneRecently();
 #else
         GCController::singleton().garbageCollectSoon();