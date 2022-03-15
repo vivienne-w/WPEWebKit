95ca4d96e074d074b90a50e9d3cbb355f03d0426
David Koo <david.koo@broadcom.com>
2022-03-15 10:15:00 -0400

    Disable Broadcom brcmtsdemux plugin (#810)
    Brcmtsdemux is a hardware demux only to be used by playersinkbin for linear playback.
    hlsdemux or dashdemux would not work properly with brcmtsdemux.

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -253,6 +253,17 @@ void MediaPlayerPrivateGStreamer::setPlaybinURL(const URL& url)
 
 void MediaPlayerPrivateGStreamer::load(const String& urlString)
 {
+#if PLATFORM(BCM_NEXUS) || PLATFORM(BROADCOM)
+    GstRegistry* reg = gst_registry_get();
+    GstPluginFeature *feature = gst_registry_lookup_feature(reg, "brcmtsdemux");
+
+    if(feature) {
+        gst_plugin_feature_set_rank(feature, GST_RANK_NONE);
+        gst_object_unref(feature);
+        GST_INFO("brcmtsdemux is disabled\n");
+    }
+#endif
+
     loadFull(urlString, nullptr, String());
 }
 
