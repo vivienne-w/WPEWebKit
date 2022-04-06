yurikoles/qtwebkit

[GTK][WPE] Web Inspector: make it possible to use the remote inspector from other browsers https://bugs.webkit.org/show_bug.cgi?id=237601
Reviewed by Michael Catanzaro.

Source/JavaScriptCore:

Make it possible to start the inspector server with 0 port, meaning any free one will be used.

* inspector/remote/glib/RemoteInspectorServer.cpp:
(Inspector::RemoteInspectorServer::start): Pass the effective address to g_socket_listener_add_address and get the port.
* inspector/remote/glib/RemoteInspectorServer.h:
(Inspector::RemoteInspectorServer::port const): Return the port the server is listening to.

Source/WebInspectorUI:

Add Base/BrowserInspectorFrontendHost.js to be used when inspector is run and InspectorFrontendHost is not
defined in window. This new file is only included in the inspector sources for GTK and WPE ports.

* CMakeLists.txt:
* PlatformWPE: Added
* PlatformGTK:
* Scripts/combine-resources.pl: Add --input-script-name to be able to skip individual scripts too.
* Scripts/copy-user-interface-resources.pl: If inspector frontend host should not be included, remove the script
from generated files.
* UserInterface/Base/BrowserInspectorFrontendHost.js: Added.
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get supportsShowCertificate):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get isRemote):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get inspectionLevel):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get debuggableInfo):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get port):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get platform):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get platformVersionName):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get supportsDiagnosticLogging):
(wind

Source/WebKit/UIProcess/Inspector/glib/RemoteInspectorClient.cpp
@@ -30,6 +30,7 @@
 
 #include "APIDebuggableInfo.h"
 #include "APIInspectorConfiguration.h"
+#include "RemoteInspectorHTTPServer.h"
 #include "RemoteWebInspectorUIProxy.h"
 #include <JavaScriptCore/RemoteInspectorUtils.h>
 #include <WebCore/InspectorDebuggableType.h>
@@ -44,22 +45,25 @@ class RemoteInspectorProxy final : public RemoteWebInspectorUIProxyClient {
     WTF_MAKE_FAST_ALLOCATED();
 public:
     RemoteInspectorProxy(RemoteInspectorClient& inspectorClient, uint64_t connectionID, uint64_t targetID)
-        : m_proxy(RemoteWebInspectorUIProxy::create())
-        , m_inspectorClient(inspectorClient)
+        : m_inspectorClient(inspectorClient)
         , m_connectionID(connectionID)
         , m_targetID(targetID)
     {
-        m_proxy->setClient(this);
     }
 
     ~RemoteInspectorProxy()
     {
-        m_proxy->setClient(nullptr);
-        m_proxy->invalidate();
+        if (m_proxy) {
+            m_proxy->setClient(nullptr);
+            m_proxy->invalidate();
+        } else
+            RemoteInspectorHTTPServer::singleton().targetDidClose(m_connectionID, m_targetID);
     }
 
     void load(Inspector::DebuggableType debuggableType)
     {
+        m_proxy = RemoteWebInspectorUIProxy::create();
+        m_proxy->setClient(this);
         // FIXME <https://webkit.org/b/205536>: this should infer more useful data about the debug target.
         Ref<API::DebuggableInfo> debuggableInfo = API::DebuggableInfo::create(DebuggableInfoData::empty());
         debuggableInfo->setDebuggableType(debuggableType);
@@ -68,13 +72,15 @@ public:
 
     void show()
     {
-        m_proxy->show();
+        if (m_proxy)
+            m_proxy->show();
     }
 
     void setTargetName(const CString& name)
     {
 #if PLATFORM(GTK)
-        m_proxy->updateWindowTitle(name);
+        if (m_proxy)
+            m_proxy->updateWindowTitle(name);
 #endif
     }
 
@@ -82,7 +88,10 @@ public:
 
     void sendMessageToFrontend(const String& message)
     {
-        m_proxy->sendMessageToFrontend(message);
+        if (m_proxy)
+            m_proxy->sendMessageToFrontend(message);
+        else
+            RemoteInspectorHTTPServer::singleton().sendMessageToFrontend(m_connectionID, m_targetID, message);
     }
 
     void sendMessageToBackend(const String& message) override
@@ -101,7 +110,7 @@ public:
     }
 
 private:
-    Ref<RemoteWebInspectorUIProxy> m_proxy;
+    RefPtr<RemoteWebInspectorUIProxy> m_proxy;
     RemoteInspectorClient& m_inspectorClient;
     uint64_t m_connectionID;
     uint64_t m_targetID;
@@ -220,7 +229,7 @@ static Inspector::DebuggableType debuggableType(const String& targetType)
     RELEASE_ASSERT_NOT_REACHED();
 }
 
-void RemoteInspectorClient::inspect(uint64_t connectionID, uint64_t targetID, const String& targetType)
+void RemoteInspectorClient::inspect(uint64_t connectionID, uint64_t targetID, const String& targetType, InspectorType inspectorType)
 {
     auto addResult = m_inspectorProxyMap.ensure(std::make_pair(connectionID, targetID), [this, connectionID, targetID] {
         return makeUnique<RemoteInspectorProxy>(*this, connectionID, targetID);
@@ -231,7 +240,8 @@ void RemoteInspectorClient::inspect(uint64_t connectionID, uint64_t targetID, co
     }
 
     m_socketConnection->sendMessage("Setup", g_variant_new("(tt)", connectionID, targetID));
-    addResult.iterator->value->load(debuggableType(targetType));
+    if (inspectorType == InspectorType::UI)
+        addResult.iterator->value->load(debuggableType(targetType));
 }
 
 void RemoteInspectorClient::sendMessageToBackend(uint64_t connectionID, uint64_t targetID, const String& message)
@@ -278,6 +288,62 @@ void RemoteInspectorClient::sendMessageToFrontend(uint64_t connectionID, uint64_
     if (!proxy)
         return;
     proxy->sendMessageToFrontend(String::fromUTF8(message));
+}
+
+GString* RemoteInspectorClient::buildTargetListPage(InspectorType inspectorType) const
+{
+    GString* html = g_string_new(
+        "<html><head><title>Remote inspector</title>"
+        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />"
+        "<style>"
+        "  h1 { color: #babdb6; text-shadow: 0 1px 0 white; margin-bottom: 0; }"
+        "  html { font-family: -webkit-system-font; font-size: 11pt; color: #2e3436; padding: 20px 20px 0 20px; background-color: #f6f6f4; "
+        "         background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #eeeeec), color-stop(1, #f6f6f4));"
+        "         background-size: 100% 5em; background-repeat: no-repeat; }"
+        "  table { width: 100%; border-collapse: collapse; }"
+        "  table, td { border: 1px solid #d3d7cf; border-left: none; border-right: none; }"
+        "  p { margin-bottom: 30px; }"
+        "  td { padding: 15px; }"
+        "  td.data { width: 200px; }"
+        "  .targetname { font-weight: bold; }"
+        "  .targeturl { color: #babdb6; }"
+        "  td.input { width: 64px; }"
+        "  input { width: 100%; padding: 8px; }"
+        "</style>"
+        "</head><body><h1>Inspectable targets</h1>");
+    if (m_targets.isEmpty())
+        g_string_append(html, "<p>No targets found</p>");
+    else {
+        g_string_append(html, "<table>");
+        for (auto connectionID : m_targets.keys()) {
+            for (auto& target : m_targets.get(connectionID)) {
+                g_string_append_printf(html,
+                    "<tbody><tr>"
+                    "<td class=\"data\"><div class=\"targetname\">%s</div><div class=\"targeturl\">%s</div></td>"
+                    "<td class=\"input\"><input type=\"button\" value=\"Inspect\" onclick=",
+                    target.name.data(), target.url.data());
+
+                switch (inspectorType) {
+                case InspectorType::UI:
+                    g_string_append_printf(html, "\"window.webkit.messageHandlers.inspector.postMessage('%" G_GUINT64_FORMAT ":%" G_GUINT64_FORMAT ":%s');\"",
+                        connectionID, target.id, target.type.data());
+                    break;
+                case InspectorType::HTTP:
+                    g_string_append_printf(html,
+                        "\"window.open('Main.html?ws=' + window.location.host + '/socket/%" G_GUINT64_FORMAT "/%" G_GUINT64_FORMAT "/%s', "
+                        "'_blank', 'location=no,menubar=no,status=no,toolbar=no');\"",
+                        connectionID, target.id, target.type.data());
+                    break;
+                }
+
+                g_string_append(html, "></td></tr></tbody>");
+            }
+        }
+        g_string_append(html, "</table>");
+    }
+    g_string_append(html, "</body></html>");
+
+    return html;
 }
 
 } // namespace WebKit

vivienne-w/WPEWebKit

Source/WebKit/UIProcess/Inspector/glib/RemoteInspectorClient.cpp
@@ -29,6 +29,7 @@
 #if ENABLE(REMOTE_INSPECTOR)
 
 #include "APIDebuggableInfo.h"
+#include "RemoteInspectorHTTPServer.h"
 #include "RemoteWebInspectorProxy.h"
 #include <JavaScriptCore/RemoteInspectorUtils.h>
 #include <WebCore/InspectorDebuggableType.h>
@@ -43,22 +44,25 @@ class RemoteInspectorProxy final : public RemoteWebInspectorProxyClient {
     WTF_MAKE_FAST_ALLOCATED();
 public:
     RemoteInspectorProxy(RemoteInspectorClient& inspectorClient, uint64_t connectionID, uint64_t targetID)
-        : m_proxy(RemoteWebInspectorProxy::create())
-        , m_inspectorClient(inspectorClient)
+        : m_inspectorClient(inspectorClient)
         , m_connectionID(connectionID)
         , m_targetID(targetID)
     {
-        m_proxy->setClient(this);
     }
 
     ~RemoteInspectorProxy()
     {
-        m_proxy->setClient(nullptr);
-        m_proxy->invalidate();
+        if (m_proxy) {
+            m_proxy->setClient(nullptr);
+            m_proxy->invalidate();
+        } else
+            RemoteInspectorHTTPServer::singleton().targetDidClose(m_connectionID, m_targetID);
     }
 
     void load(Inspector::DebuggableType debuggableType)
     {
+        m_proxy = RemoteWebInspectorProxy::create();
+        m_proxy->setClient(this);
         // FIXME <https://webkit.org/b/205536>: this should infer more useful data about the debug target.
         Ref<API::DebuggableInfo> debuggableInfo = API::DebuggableInfo::create(DebuggableInfoData::empty());
         debuggableInfo->setDebuggableType(debuggableType);
@@ -67,19 +71,24 @@ public:
 
     void show()
     {
-        m_proxy->show();
+        if (m_proxy)
+            m_proxy->show();
     }
 
     void setTargetName(const CString& name)
     {
 #if PLATFORM(GTK)
-        m_proxy->updateWindowTitle(name);
+        if (m_proxy)
+            m_proxy->updateWindowTitle(name);
 #endif
     }
 
     void sendMessageToFrontend(const String& message)
     {
-        m_proxy->sendMessageToFrontend(message);
+        if (m_proxy)
+            m_proxy->sendMessageToFrontend(message);
+        else
+            RemoteInspectorHTTPServer::singleton().sendMessageToFrontend(m_connectionID, m_targetID, message);
     }
 
     void sendMessageToBackend(const String& message) override
@@ -93,7 +102,7 @@ public:
     }
 
 private:
-    Ref<RemoteWebInspectorProxy> m_proxy;
+    RefPtr<RemoteWebInspectorProxy> m_proxy;
     RemoteInspectorClient& m_inspectorClient;
     uint64_t m_connectionID;
     uint64_t m_targetID;
@@ -214,7 +223,7 @@ static Inspector::DebuggableType debuggableType(const String& targetType)
     RELEASE_ASSERT_NOT_REACHED();
 }
 
-void RemoteInspectorClient::inspect(uint64_t connectionID, uint64_t targetID, const String& targetType)
+void RemoteInspectorClient::inspect(uint64_t connectionID, uint64_t targetID, const String& targetType, InspectorType inspectorType)
 {
     auto addResult = m_inspectorProxyMap.ensure(std::make_pair(connectionID, targetID), [this, connectionID, targetID] {
         return makeUnique<RemoteInspectorProxy>(*this, connectionID, targetID);
@@ -225,7 +234,8 @@ void RemoteInspectorClient::inspect(uint64_t connectionID, uint64_t targetID, co
     }
 
     m_socketConnection->sendMessage("Setup", g_variant_new("(tt)", connectionID, targetID));
-    addResult.iterator->value->load(debuggableType(targetType));
+    if (inspectorType == InspectorType::UI)
+        addResult.iterator->value->load(debuggableType(targetType));
 }
 
 void RemoteInspectorClient::sendMessageToBackend(uint64_t connectionID, uint64_t targetID, const String& message)
@@ -272,6 +282,62 @@ void RemoteInspectorClient::sendMessageToFrontend(uint64_t connectionID, uint64_
     if (!proxy)
         return;
     proxy->sendMessageToFrontend(String::fromUTF8(message));
+}
+
+GString* RemoteInspectorClient::buildTargetListPage(InspectorType inspectorType) const
+{
+    GString* html = g_string_new(
+        "<html><head><title>Remote inspector</title>"
+        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />"
+        "<style>"
+        "  h1 { color: #babdb6; text-shadow: 0 1px 0 white; margin-bottom: 0; }"
+        "  html { font-family: -webkit-system-font; font-size: 11pt; color: #2e3436; padding: 20px 20px 0 20px; background-color: #f6f6f4; "
+        "         background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #eeeeec), color-stop(1, #f6f6f4));"
+        "         background-size: 100% 5em; background-repeat: no-repeat; }"
+        "  table { width: 100%; border-collapse: collapse; }"
+        "  table, td { border: 1px solid #d3d7cf; border-left: none; border-right: none; }"
+        "  p { margin-bottom: 30px; }"
+        "  td { padding: 15px; }"
+        "  td.data { width: 200px; }"
+        "  .targetname { font-weight: bold; }"
+        "  .targeturl { color: #babdb6; }"
+        "  td.input { width: 64px; }"
+        "  input { width: 100%; padding: 8px; }"
+        "</style>"
+        "</head><body><h1>Inspectable targets</h1>");
+    if (m_targets.isEmpty())
+        g_string_append(html, "<p>No targets found</p>");
+    else {
+        g_string_append(html, "<table>");
+        for (auto connectionID : m_targets.keys()) {
+            for (auto& target : m_targets.get(connectionID)) {
+                g_string_append_printf(html,
+                    "<tbody><tr>"
+                    "<td class=\"data\"><div class=\"targetname\">%s</div><div class=\"targeturl\">%s</div></td>"
+                    "<td class=\"input\"><input type=\"button\" value=\"Inspect\" onclick=",
+                    target.name.data(), target.url.data());
+
+                switch (inspectorType) {
+                case InspectorType::UI:
+                    g_string_append_printf(html, "\"window.webkit.messageHandlers.inspector.postMessage('%" G_GUINT64_FORMAT ":%" G_GUINT64_FORMAT ":%s');\"",
+                        connectionID, target.id, target.type.data());
+                    break;
+                case InspectorType::HTTP:
+                    g_string_append_printf(html,
+                        "\"window.open('Main.html?ws=' + window.location.host + '/socket/%" G_GUINT64_FORMAT "/%" G_GUINT64_FORMAT "/%s', "
+                        "'_blank', 'location=no,menubar=no,status=no,toolbar=no');\"",
+                        connectionID, target.id, target.type.data());
+                    break;
+                }
+
+                g_string_append(html, "></td></tr></tbody>");
+            }
+        }
+        g_string_append(html, "</table>");
+    }
+    g_string_append(html, "</body></html>");
+
+    return html;
 }
 
 } // namespace WebKit