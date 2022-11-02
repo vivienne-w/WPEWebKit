1850751afdf512a221a066e4bffa8439d8430a04
Pawel Lampe <pawel.lampe@gmail.com>
2022-11-02 03:48:33 -0700

    [MSE] Bring back some useful SourceBuffer logs https://bugs.webkit.org/show_bug.cgi?id=247122
    Reviewed by Xabier Rodriguez-Calvar.
    
    Changeset https://trac.webkit.org/changeset/241148/webkit from
    https://bugs.webkit.org/show_bug.cgi?id=194348 removed line
    https://github.com/WebKit/WebKit/commit/cd4e7ebee1a6aaa956534cb8709d95ec2c75b85a#diff-78280e252e732edbf1f9b6517bd2f98eb1475f16fc781a5f3e2e2e6bf9b2a9beL710
    , which was very useful when debugging MSE append problems.
    
    This patch is bringing that line back and adding a similar one
    to appendBufferInternal(), which will be helpful to track how
    the buffered ranges evolve as the append is processed.
    
    Original author: Pawel Lampe <pawel.lampe@gmail.com>
    See: https://github.com/WebPlatformForEmbedded/WPEWebKit/pull/955
    
    * Source/WebCore/Modules/mediasource/SourceBuffer.cpp:
    (WebCore::SourceBuffer::appendBufferInternal): Added log line.
    (WebCore::SourceBuffer::sourceBufferPrivateAppendComplete): Ditto.
    
    Canonical link: https://commits.webkit.org/256224@main

--- Source/WebCore/Modules/mediasource/SourceBuffer.cpp
+++ Source/WebCore/Modules/mediasource/SourceBuffer.cpp
@@ -58,6 +58,7 @@
 #include <limits>
 #include <wtf/CheckedArithmetic.h>
 #include <wtf/IsoMallocInlines.h>
+#include <wtf/StringPrintStream.h>
 #include <wtf/WeakPtr.h>
 
 namespace WebCore {
@@ -595,6 +596,10 @@ ExceptionOr<void> SourceBuffer::appendBufferInternal(const unsigned char* data, 
     if (isRemoved() || m_updating)
         return Exception { InvalidStateError };
 
+    StringPrintStream message;
+    message.printf("SourceBuffer::appendBufferInternal(%p) - append size = %u, buffered = %s\n", this, size, toString(m_private->buffered()->ranges()).utf8().data());
+    DEBUG_LOG(LOGIDENTIFIER, message.toString());
+
     // 3. If the readyState attribute of the parent media source is in the "ended" state then run the following steps:
     // 3.1. Set the readyState attribute of the parent media source to "open"
     // 3.2. Queue a task to fire a simple event named sourceopen at the parent media source .
@@ -718,7 +723,9 @@ void SourceBuffer::sourceBufferPrivateAppendComplete(AppendResult result)
     if (extraMemoryCost() > this->maximumBufferSize())
         m_bufferFull = true;
 
-    DEBUG_LOG(LOGIDENTIFIER);
+    StringPrintStream message;
+    message.printf("SourceBuffer::sourceBufferPrivateAppendComplete(%p) - buffered = %s", this, toString(m_private->buffered()->ranges()).utf8().data());
+    DEBUG_LOG(LOGIDENTIFIER, message.toString());
 }
 
 void SourceBuffer::sourceBufferPrivateDidReceiveRenderingError(int error)
