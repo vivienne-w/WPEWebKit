a617d05cbf98b08ab897d62169fbbbb62fb4358c
Xabier Rodriguez-Calvar <calvaris@igalia.com>
2022-11-14 04:57:20 -0800

    [GStreamer] Video sink should discard late frames to avoid audio and video desynchronization https://bugs.webkit.org/show_bug.cgi?id=247740
    Reviewed by Philippe Normand.
    
    In some cases and/or platforms video frames can arrive late and if are not discarded, they end up being painted and
    there can be a/v desynchronization. Setting the max-lateness property forces the sink to discard late frames and solve
    the problem.
    
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::createVideoSink):
    
    Canonical link: https://commits.webkit.org/256650@main

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -88,6 +88,7 @@
 #include <wtf/MathExtras.h>
 #include <wtf/MediaTime.h>
 #include <wtf/NeverDestroyed.h>
+#include <wtf/Scope.h>
 #include <wtf/StringPrintStream.h>
 #include <wtf/text/AtomString.h>
 #include <wtf/text/CString.h>
@@ -3574,6 +3575,25 @@ void MediaPlayerPrivateGStreamer::pushNextHolePunchBuffer()
 GstElement* MediaPlayerPrivateGStreamer::createVideoSink()
 {
     acceleratedRenderingStateChanged();
+
+    // Ensure the sink has the max-lateness property set.
+    auto exit = makeScopeExit([this] {
+        ASSERT(m_videoSink);
+        // FIXME: We would not enable this for MediaStream.
+
+        GstElement* sink = m_videoSink.get();
+        while (GST_IS_BIN(sink)) {
+            GUniquePtr<GstIterator> iter(gst_bin_iterate_sinks(GST_BIN_CAST(sink)));
+            GValue value = G_VALUE_INIT;
+            auto result = gst_iterator_next(iter.get(), &value);
+            ASSERT_UNUSED(result, result == GST_ITERATOR_OK);
+            sink = GST_ELEMENT(g_value_get_object(&value));
+            g_value_unset(&value);
+        }
+
+        uint64_t maxLateness = 100 * GST_MSECOND;
+        g_object_set(sink, "max-lateness", maxLateness, nullptr);
+    });
 
 #if USE(GSTREAMER_HOLEPUNCH)
     m_videoSink = createHolePunchVideoSink();
